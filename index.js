73981327a180515ec1c62c3db6d567c694ae88ce
kaiyumdev <afrankaiyum34@gmail.com>
2024-12-18 12:58:32 +0600

    start hotels-rooftop-server

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+const port = process.env.port || 5000;
+
+
+app.get('/', (req, res) => {
+    res.send('Hotels Rooftop server is running.....!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
