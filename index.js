b5f66ab0c0b1379da3d02ee5164604988808bb10
kaiyumdev <afrankaiyum34@gmail.com>
2024-12-18 20:50:59 +0600

    install mongodb, mongoose, corse & dotenv and also connect with mongodb database

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
 const port = process.env.port || 5000;
 
+app.use(express.json());
+app.use(cors());
+async function main() {
+    await mongoose.connect(process.env.MONGODB_URL);
+};
 
+main().then(() => console.log('connect mongodb successfully')).catch(err => console.log(err));
+
+
+//afrankaiyum34
+//Mscln4t1wEHGlkaU
+//hotels-rooftop
 app.get('/', (req, res) => {
     res.send('Hotels Rooftop server is running.....!')
 })
