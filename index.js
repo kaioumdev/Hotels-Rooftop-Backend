62d7eee215300179a0702f0d96db81bcdcdb2658
kaiyumdev <afrankaiyum34@gmail.com>
2024-12-19 17:22:44 +0600

    get method add tot he main function

--- index.js
+++ index.js
@@ -9,17 +9,12 @@ app.use(express.json());
 app.use(cors());
 async function main() {
     await mongoose.connect(process.env.MONGODB_URL);
+    app.get('/', (req, res) => {
+        res.send('Hotels Rooftop server is running.....!')
+    })
 };
 
 main().then(() => console.log('connect mongodb successfully')).catch(err => console.log(err));
-
-
-//afrankaiyum34
-//Mscln4t1wEHGlkaU
-//hotels-rooftop
-app.get('/', (req, res) => {
-    res.send('Hotels Rooftop server is running.....!')
-})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
