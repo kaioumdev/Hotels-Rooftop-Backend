8823fab06eda34175043a50942d6a215e9bc6da1
kaiyumdev <afrankaiyum34@gmail.com>
2024-12-19 17:34:44 +0600

    connect blogRoutes at index.js file

--- index.js
+++ index.js
@@ -5,8 +5,13 @@ const cors = require('cors');
 require('dotenv').config();
 const port = process.env.port || 5000;
 
+//parse options
 app.use(express.json());
 app.use(cors());
+
+//routes
+const blogRoutes = require("./src/routes/blog.route");
+app.use("/api/blogs", blogRoutes);
 async function main() {
     await mongoose.connect(process.env.MONGODB_URL);
     app.get('/', (req, res) => {
