d1a232c898440fdb82702b58d0e0656710e48cea
kaiyumdev <afrankaiyum34@gmail.com>
2024-12-19 18:59:09 +0600

    create new post route

--- src/routes/blog.route.js
+++ src/routes/blog.route.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
+//create blog post
+router.post("/create-post", async (req, res) => {
+    try {
+        console.log("Blog data from api", req.body)
+    } catch (error) {
+        console.error("Error creating post", error);
+        res.status(500).send("Server Error");
+    }
+})
+
+
+//get all blogs
 router.get("/", async (req, res) => {
     res.send("Blog Routes running...");
 })
