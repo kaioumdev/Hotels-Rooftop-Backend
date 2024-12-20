9d690b567a4c8f1f223f6d30093714c6f4626acc
kaiyumdev <afrankaiyum34@gmail.com>
2024-12-20 22:05:56 +0600

    complete register user to create new user successfully

--- src/routes/auth.user.route.js
+++ src/routes/auth.user.route.js
@@ -1,5 +1,18 @@
 const express = require('express');
+const User = require('../model/user.model');
 const router = express.Router();
+
+//register a new user
+router.post("/register", async (req, res) => {
+    try {
+        const { email, password, username } = req.body;
+        const user = new User({ email, password, username });
+        console.log(user);
+    } catch (error) {
+        console.error("Failed to register user", error);
+        res.status(500).send("Registration failed");
+    }
+})
 
 
 module.exports = router;
