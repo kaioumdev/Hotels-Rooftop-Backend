e59b140101cda4a9c03284bf7119501e82ec72a9
kaiyumdev <afrankaiyum34@gmail.com>
2024-12-22 18:19:20 +0600

    updated generateToken function

--- src/middleware/generateToken.js
+++ src/middleware/generateToken.js
@@ -1,4 +1,15 @@
 const jwt = require('jsonwebtoken');
-const generateToken = (userId) => {
-
+const User = require('../model/user.model');
+const JWT_SECRET = process.env.JWT_SECRET_KEY;
+const generateToken = async (userId) => {
+    try {
+        const user = await User.findById(userId);
+        if (!user) {
+            throw new Error('User not found');
+        };
+        const token = jwt.sign({ userId: user._id, role: user.role }, JWT_SECRET, { expiresIn: '1h' })
+    } catch (error) {
+        console.error("Error generating token: " + error)
+        throw error;
+    }
 }
