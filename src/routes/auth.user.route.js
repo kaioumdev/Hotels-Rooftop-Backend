8e92e484c492cd85e22709b3840d1f18cc0ed0c9
kaiyumdev <afrankaiyum34@gmail.com>
2024-12-23 17:23:45 +0600

    complete put method for update user role as admin

--- src/routes/auth.user.route.js
+++ src/routes/auth.user.route.js
@@ -86,6 +86,22 @@ router.delete("/users/:id", async (req, res) => {
         console.error("Failed to delete user", error);
         res.status(500).send("Failed to delete user");
     }
+});
+
+//update a user role
+router.put("/users/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { role } = req.body;
+        const user = await User.findByIdAndUpdate(id, { role }, { new: true });
+        if (!user) {
+            return res.status(404).send({ message: "User not found" });
+        };
+        res.status(200).send({ message: "User role updated successfully", user });
+    } catch (error) {
+        console.error("Failed to update user role", error);
+        res.status(500).send("Failed to update user role");
+    }
 })
 
 
