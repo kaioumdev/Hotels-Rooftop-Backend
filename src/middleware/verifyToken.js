9ecc4746d9027cf369af22ef63b0ab6f656d8a56
kaiyumdev <afrankaiyum34@gmail.com>
2024-12-29 20:00:47 +0600

    complete verifyToken function & work successfully

--- src/middleware/verifyToken.js
+++ src/middleware/verifyToken.js
@@ -2,9 +2,17 @@ const jwt = require('jsonwebtoken');
 
 const verifyToken = (req, res, next) => {
     try {
-        const token2 = req.headers['authorization'];
-        // const token = req.cookies.token;
-        console.log(token2);
+        const token = req.headers.authorization?.split(' ')[1];
+        if (!token) {
+            return res.status(401).json({ message: 'No token provided' });
+        };
+        const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+        if (!decoded) {
+            return res.status(401).json({ message: 'Invalid token' });
+        };
+        req.userId = decoded.userId;
+        req.role = decoded.role;
+        next();
     } catch (error) {
         console.error("Error verifying token: " + error);
         res.status(500).send({ message: 'Invalid token' });
