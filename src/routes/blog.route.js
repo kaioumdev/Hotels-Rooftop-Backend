25cede0530587a9bc4f5783ab59e6e3f9dad12f3
kaiyumdev <afrankaiyum34@gmail.com>
2024-12-29 18:16:59 +0600

    remove author from creating newPost

--- src/routes/blog.route.js
+++ src/routes/blog.route.js
@@ -6,7 +6,7 @@ const router = express.Router();
 //create blog post
 router.post("/create-post", async (req, res) => {
     try {
-        const newPost = new Blog({ ...req.body, author: req.userId });
+        const newPost = new Blog({ ...req.body });
         await newPost.save();
         res.status(201).send({ message: "New post created successfully", post: newPost })
     } catch (error) {
