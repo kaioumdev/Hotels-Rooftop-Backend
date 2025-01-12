6563fc18cde20ad0e97bc757bc3176f7d6957b50
kaiyumdev <afrankaiyum34@gmail.com>
2025-01-12 15:26:32 +0600

    use bodyParser & cookies

--- src/middleware/verifyToken.js
+++ src/middleware/verifyToken.js
@@ -2,7 +2,8 @@ const jwt = require('jsonwebtoken');
 
 const verifyToken = (req, res, next) => {
     try {
-        const token = req.headers.authorization?.split(' ')[1];
+        const token = req.cookies.token;
+        // const token = req.headers.authorization?.split(' ')[1];
         if (!token) {
             return res.status(401).json({ message: 'No token provided' });
         };
