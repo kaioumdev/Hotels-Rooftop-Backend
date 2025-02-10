2333cb910581a3fc51d6d029435b483242832bcf
kaiyumdev <afrankaiyum34@gmail.com>
2025-02-10 16:09:17 +0600

    feat(login): got login info & login user successfully

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -15,7 +15,6 @@ const registerUser = async (req, res) => {
 
 const loginUser = async (req, res) => {
     try {
-        // console.log(req.body);
         const { email, password } = req.body;
         const user = await User.findOne({ email: email });
         if (!user) {
