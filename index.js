2431c46255f28a25921305b855ccf1e30cd02038
kaiyumdev <afrankaiyum34@gmail.com>
2025-02-16 19:15:05 +0600

    updated header to add Access-Control-Allow-Origin

--- index.js
+++ index.js
@@ -18,10 +18,18 @@ app.use(bodyParser.json({ limit: '10mb' }));
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
 app.use(cors({
     origin: 'https://hotels-rooftop-frontend.vercel.app',
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    methods: ["GET", "POST", "PUT", "PATCH", "PUT", "DELETE"],
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"]
 }));
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "https://hotels-rooftop-frontend.vercel.app"); // ✅ Set frontend URL
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
+});
 
 
 // Routes
