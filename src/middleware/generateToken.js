8e4430e70a9660cc8475461739c5db828c21a76a
kaiyumdev <afrankaiyum34@gmail.com>
2025-02-17 19:07:54 +0600

    feat(generateToken): remove commit & usual code from generateToken file

--- src/middleware/generateToken.js
+++ src/middleware/generateToken.js
@@ -1,23 +1,3 @@
-// const jwt = require('jsonwebtoken');
-// const User = require('../model/user.model');
-// const JWT_SECRET = process.env.JWT_SECRET_KEY;
-// const generateToken = async (userId) => {
-//     try {
-//         const user = await User.findById(userId);
-//         if (!user) {
-//             throw new Error('User not found');
-//         };
-//         const token = jwt.sign({ userId: user._id, role: user.role }, JWT_SECRET, { expiresIn: '1d' });
-//         return token;
-//     } catch (error) {
-//         console.error("Error generating token: " + error)
-//         throw error;
-//     }
-// };
-
-// module.exports = generateToken;
-
-
 const jwt = require('jsonwebtoken');
 const User = require('../model/user.model');
 const JWT_SECRET = process.env.JWT_SECRET_KEY;
